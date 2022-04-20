3cfe5a879e84fa2b2aab9236c8091eb20693ae6e
veysell <vdrn76@gmail.com>
2022-04-20 03:10:41 +0300

    category and product repository added

---
+++ BusinnesLayer/Concrete/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinnesLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnesLayer.Concrete
+{
+    public class CategoryRepository:GenericRepository<Category>
+    {
+
+    }
+}
